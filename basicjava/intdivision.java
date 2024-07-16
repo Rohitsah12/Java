b8d4f7ef21b435c017d1ebf4d2fb3d6c3b4a9eca
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-07-16 15:39:04 +0530

    integer division

---
+++ basicjava/intdivision.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class QuotientRemainder{
+    void operation(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the numerator: ");
+        int numerator=sc.nextInt();
+        System.out.println("Enter the denominator: ");
+        int denominator=sc.nextInt();
+        int quotient=numerator/denominator;
+        int remainder=numerator%denominator;
+        System.out.println("The quotient is: "+quotient);
+        System.out.println("The remainder is: "+remainder);
+    }
+}
+public class intdivision {
+    public static void main(String[] args) {
+        QuotientRemainder a=new QuotientRemainder();
+        a.operation();
+    }
+}
